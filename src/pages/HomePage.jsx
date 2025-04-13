69c71d72c33ca814d4d53f5c88c8b4b96993823f
Krishna Jaiswal <krishna.jaiswal@vitstudent.ac.in>
2025-04-13 21:10:41 +0530

    collection
    collection

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import jungleImg from "../assets/collections/jungledrop.jpeg";
+import wildImg from "../assets/collections/wildwithin.jpeg";
 // import "../styles/home.css";
 import "../styles/global.css";
 import "../styles/navbar.css";
@@ -12,6 +14,11 @@ import "../styles/responsive.css";
 import "../styles/collections.css";
 import Navbar from "../components/Navbar";
 import { API } from "../config/api"; // ✅ import centralized API
+
+const collectionImages = {
+    "Jungle Drop": jungleImg,
+    "Wild Drop": wildImg,
+}
 
 export default function HomePage() {
   const [collections, setCollections] = useState([]);
@@ -33,14 +40,15 @@ export default function HomePage() {
     <div className="homepage">
       <Navbar />
       <main className="homepage-main">
-        <h2 className="collections-title">Our Collections</h2>
+        {/* <h2 className="collections-title">Our Collections</h2> */}
         <div className="collections-grid">
           {collections.map((collection) => (
             <div key={collection.id} className="collection-item">
               <Link to={`/collection/${collection.name}`}>
                 <div className="collection-card">
                   <img
-                    src={collection.imageUrl}
+                    // src={collection.imageUrl}
+                    src={collectionImages[collection.name]}
                     alt={collection.name}
                     className="collection-image"
                   />
